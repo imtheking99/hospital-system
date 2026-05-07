489cf26af232c5c0d0879f14044e72ecb7096bfc
imtheking99 <kumarabp3@gmail.com>
2026-05-07 10:30:18 +0530

    new reg link added

--- index.php
+++ index.php
@@ -19,6 +19,10 @@
                 <input type="password" name="password" required>
             </div>
             <button type="submit" name="login_btn">Sign In</button>
+            
+            <div class="register-link">
+                <p>Not yet a member? <a href="register.php">Register Today</a></p>
+            </div>
         </form>
     </div>
 </body>
