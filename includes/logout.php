ca72f96cf00e96ef02990d9b8a1b3ca435abc582
shavin-b <bandarasavin@gmail.com>
2026-05-07 13:15:18 +0530

    Implement Patient Dashboard and finalize Authentication logic Created patient_dashboard.php with appointment booking form. Developed patient_dashboard.php to display booking history using MySQL Views. Fixed path errors in includes/aphandler.php and auth.php. Added logout.php for secure session management. Integrated stored procedure (sp_BookAppointment) for booking logic.

---
+++ includes/logout.php
@@ -0,0 +1,15 @@
+<?php
+// start Session 
+session_start();
+`
+//erase all session variables
+session_unset();
+
+//destroy session
+session_destroy();
+
+//redirect to login page with a status message
+
+header("Location: ../index.php?status=loggedout");
+exit();
+?>
